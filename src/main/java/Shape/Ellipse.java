62b81bff9139afed09ea7e6d3b52a245ea489ed5
Felix <felix.paille-dowell@polymtl.ca>
2024-02-03 14:23:06 -0500

    ellipse

--- src/main/java/Shape/Ellipse.java
+++ src/main/java/Shape/Ellipse.java
@@ -11,7 +11,40 @@ public class Ellipse extends BaseShape {
      * @param heightDiameter Height of the Ellipse
      */
     public Ellipse(Double widthDiameter, Double heightDiameter) {
-        
+        super();
+
+        Double xi = widthDiameter/2;
+        Double hxh = heightDiameter*heightDiameter;
+        Double wxw = widthDiameter*widthDiameter;
+        Double hhxww = hxh*wxw;
+        Double slope = 0.0;
+
+        // Creer l'axe du milieu
+        for (Double x = -widthDiameter/2; x <= widthDiameter/2; x+= 0.5){
+            this.add(new Point2d(x, 0.0));
+        }
+
+        //Les deux moities
+        for (Double y = 0.5; y <= heightDiameter/2; y += 0.5){
+            Double xf = xi - slope;
+            while (xf > 0){
+                if (xf*xf*hxh + y*y*wxw <= hhxww){
+                    break;
+                }
+                xf -= 0.5;
+            }
+
+            slope = xi - xf;
+            xi = xf;
+
+            for (Double x = -xi; x <= xi; x += 0.5)
+            {
+                this.add(new Point2d(x, -y));
+                this.add(new Point2d(x, y));
+            }
+        }
+
+
     }
 
     /** TODO
@@ -19,7 +52,10 @@ public class Ellipse extends BaseShape {
      * @param dimensions 2D point containing the width and height of the Ellipse
      */
     public Ellipse(Point2d dimensions) {
-
+        super();
+        Double width = dimensions.X();
+        Double height = dimensions.Y();
+        this.add(new Ellipse(width, height));
     }
 
     /**
@@ -27,7 +63,8 @@ public class Ellipse extends BaseShape {
      * @param coords Collection of 2D points
      */
     private Ellipse(Collection<Point2d> coords) {
-
+        super();
+        this.addAll(coords);
     }
 
     /** TODO
@@ -35,6 +72,6 @@ public class Ellipse extends BaseShape {
      */
     @Override
     public Ellipse clone() {
-        return null;
+        return new Ellipse(this.cloneCoords());
     }
 }
